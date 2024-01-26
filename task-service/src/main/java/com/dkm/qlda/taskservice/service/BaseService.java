7a19d2056e49bd90513658a39cab773e7b0d4c59
dkminh.hcm <dkminh.hcm@vnpt.vn>
2024-01-27 00:09:53 +0700

    [ADD]: method service

--- task-service/src/main/java/com/dkm/qlda/taskservice/service/BaseService.java
+++ task-service/src/main/java/com/dkm/qlda/taskservice/service/BaseService.java
@@ -4,4 +4,8 @@ import java.util.List;
 
 public interface BaseService<T, E> {
     List<T> findAll();
+    T findById(E id);
+    List<T> findAllById(E id);
+    int save(T object);
+    void delete(E id);
 }
--- task-service/src/main/java/com/dkm/qlda/taskservice/service/TaskService.java
+++ task-service/src/main/java/com/dkm/qlda/taskservice/service/TaskService.java
@@ -16,4 +16,24 @@ public class TaskService implements BaseService<Task, String>{
     public List<Task> findAll() {
         return taskRepository.findAll();
     }
+
+    @Override
+    public Task findById(String id) {
+        return taskRepository.findById(id).get();
+    }
+
+    @Override
+    public List<Task> findAllById(String id) {
+        return null;
+    }
+
+    @Override
+    public int save(Task object) {
+        return taskRepository.save(object) == null ? 1:0;
+    }
+
+    @Override
+    public void delete(String id) {
+        taskRepository.deleteById(id);
+    }
 }
