f61afaf9abb005f25ebce2e8f5970fc0c973754c
dkminh.hcm <dkminh.hcm@vnpt.vn>
2024-02-26 09:20:03 +0700

    fix gateway

--- api-gateway/src/main/java/com/dkm/qlda/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/dkm/qlda/apigateway/ApiGatewayApplication.java
@@ -6,8 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@ComponentScan("com.dkm.qlda.common.*")
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
